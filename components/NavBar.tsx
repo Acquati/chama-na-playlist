4e163c9831053e3e94d62d4e2a100656986b68ba
Leandro Acquati <leandroacquati@gmail.com>
2021-02-17 07:02:35 -0300

    add search bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,24 @@
 import styles from './NavBar.module.sass'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const NavBar = () => (
   <div className={styles.navBar}>
     <div className={styles.container}>
       <h1>ChamaNaPlaylist</h1>
-      <div>
-        <h1>Pesquisar</h1>
-        <div className={styles.menuToggle}>
-          <input type="checkbox" />
-          <span></span>
-          <span></span>
-          <span></span>
+
+      <div className={styles.searchContainer}>
+        <input type="text" />
+        <div className={styles.searchIcon}>
+          <FontAwesomeIcon icon={faSearch} />
         </div>
+      </div>
+
+      <div className={styles.menuToggle}>
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   </div>

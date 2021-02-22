21c6950fffdc8b60df6a786c712f359b5817fe60
Leandro Acquati <leandroacquati@gmail.com>
2021-02-22 17:11:46 -0300

    update SpotifyEmbeds

---
+++ components/SpotifyEmbeds/SpotifyEmbed/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from 'react'
+import styles from './styles.module.sass'
+
+type Props = {
+  src: string
+}
+
+const useResize = (myRef: any) => {
+  const [width, setWidth] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    setWidth(myRef.current.offsetWidth)
+    setHeight(myRef.current.offsetHeight)
+  }, [myRef])
+
+  return { width, height }
+}
+
+const SpotifyEmbed = (props: Props) => {
+  const componentRef = useRef(null)
+  const { width, height } = useResize(componentRef)
+
+  return (
+    <div ref={componentRef} className={styles.spotifyEmbed}>
+      <iframe src={props.src} width={width} height={height} frameBorder="0"></iframe>
+    </div>
+  )
+}
+
+export default SpotifyEmbed
--- components/SpotifyEmbeds/index.tsx
+++ components/SpotifyEmbeds/index.tsx
@@ -1,22 +1,15 @@
 import styles from './styles.module.sass'
+import SpotifyEmbed from './SpotifyEmbed'
 
 const SpotifyEmbeds = () => {
   return (
     <div className={styles.spotifyEmbeds}>
       <h1>Playlists Recentes</h1>
 
-      <div className={styles.spotifyEmbed}>
-        <iframe src="https://embed.spotify.com/?uri=spotify:user:oosabaj:playlist:2UDe8QqHAXUaLrdb0QsDGE" width="340" height="80" frameBorder="0"></iframe>
-      </div>
-      <div className={styles.spotifyEmbed}>
-        <iframe src="https://embed.spotify.com/?uri=spotify:user:oosabaj:playlist:51vBEL5eVtII7uIEDltWwB" width="340" height="80" frameBorder="0"></iframe>
-      </div>
-      <div className={styles.spotifyEmbed}>
-        <iframe src="https://embed.spotify.com/?uri=spotify:user:dembsky:playlist:7qfcDxieRKE6VOFE3YNKbz" width="340" height="80" frameBorder="0"></iframe>
-      </div>
-      <div className={styles.spotifyEmbed}>
-        <iframe src="https://embed.spotify.com/?uri=spotify:user:thech053none:playlist:3epVhtO7ZWOB4DzuJFY5da" width="340" height="80" frameBorder="0"></iframe>
-      </div>
+      <SpotifyEmbed src="https://embed.spotify.com/?uri=spotify:user:oosabaj:playlist:2UDe8QqHAXUaLrdb0QsDGE" />
+      <SpotifyEmbed src="https://embed.spotify.com/?uri=spotify:user:oosabaj:playlist:51vBEL5eVtII7uIEDltWwB" />
+      <SpotifyEmbed src="https://embed.spotify.com/?uri=spotify:user:dembsky:playlist:7qfcDxieRKE6VOFE3YNKbz" />
+      <SpotifyEmbed src="https://embed.spotify.com/?uri=spotify:user:thech053none:playlist:3epVhtO7ZWOB4DzuJFY5da" />
     </div>
   )
 }
